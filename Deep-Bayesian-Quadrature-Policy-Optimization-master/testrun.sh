01fa60e95d9f04b3b942598c1f37487f867c641e
pieteroyens <oyenspieter@gmail.com>
2020-10-22 13:58:17 +0200

    added bash script

---
+++ Deep-Bayesian-Quadrature-Policy-Optimization-master/testrun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#source ~/anaconda3/etc/profile.d/conda.sh
+#conda activate DBQPG
+if [ $1 != TRPO ] && [ $1 != NPG ] && [ $1 != VanillaPG ]; then
+	echo Enter a valid optimizing scheme \(VanillaPG, NPG, TRPO\)
+	Error: wrong input given	
+	exit 1
+fi
+
+
+#python agent.py --env-name $1 --pg_algorithm $2 --pg_estimator $3 --seed $4
+#python agent.py --env-name $1 --pg_algorithm $2 --pg_estimator $3 --seed $4
+#python agent.py --env-name $1 --pg_algorithm $2 --pg_estimator $3 --seed $4
+
